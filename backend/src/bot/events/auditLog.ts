58c8d98a42dda0dc186d16b0e601a7f5398a5df7
Michal <michal@michalarch.local>
2025-07-05 23:32:38 +0200

    fix log enabled

--- backend/src/bot/events/auditLog.ts
+++ backend/src/bot/events/auditLog.ts
@@ -3,19 +3,22 @@ import { AuditLogEvent, Events, Guild, GuildAuditLogsEntry } from "discord.js";
 import logTimeout from "../utils/messages/logTimeout";
 import logBan from "../utils/messages/logBan";
 import { BotEvent } from "../../types/bot";
+import { ConfigManager } from "../utils/ConfigManager";
 
 const event: BotEvent = {
   name: Events.GuildAuditLogEntryCreate,
   async execute(auditLog: GuildAuditLogsEntry, guild: Guild) {
-    console.log("dupa");
+    const configManager = ConfigManager.getInstance();
+    const config = await configManager.get(guild);
+    console.log("config?.timeoutLogEnabled", config?.timeoutLogEnabled);
     
     switch (auditLog.action) {
       case AuditLogEvent.MemberBanAdd:
-        logBan(auditLog, guild);
+        if (config?.banLogEnabled) logBan(auditLog, guild);
         break;
 
       case AuditLogEvent.MemberUpdate:
-        logTimeout(auditLog, guild);
+        if (config?.timeoutLogEnabled) logTimeout(auditLog, guild);
         break;
 
       default:
